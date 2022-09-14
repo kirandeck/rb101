0dc98a28984d233d9901f89b820e5956ae4e1994
Kiran Deck <kiranjdeck@gmail.com>
2022-09-14 08:03:41 -0400

    tic tac toe

--- lesson_six/tic_tac_toe_copy.rb
+++ lesson_six/tic_tac_toe_copy.rb
@@ -69,13 +69,7 @@ def joinor(array, default1 = ', ', default2 = 'or')
   end
 end
 
-def computer_defense(line, board, marker)
-  if board.values_at(*line).count(marker) == 2
-    board.select { |k, v| line.include?(k) && v == INTIAL_MARKER }.keys.first
-  end
-end
-
-def computer_offense(line, board, marker)
+def find_square(line, board, marker)
   if board.values_at(*line).count(marker) == 2
     board.select { |k, v| line.include?(k) && v == INTIAL_MARKER }.keys.first
   end
@@ -92,15 +86,19 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-def computer_places_piece!(brd)
-  square = nil
+def computer_offense_available?(square, brd)
   WINNING_LINES.each do |line|
-    square = computer_offense(line, brd, COMPUTER_MARKER)
+    square = find_square(line, brd, COMPUTER_MARKER)
     break if square
   end
+end
+
+def computer_places_piece!(brd)
+  square = nil
+  computer_offense_available?(square, brd)
   if !square
     WINNING_LINES.each do |line|
-      square = computer_defense(line, brd, PLAYER_MARKER)
+      square = find_square(line, brd, PLAYER_MARKER)
       break if square
     end
   end
